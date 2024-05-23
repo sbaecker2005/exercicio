a67372eeeb28577810fcff3bd360806dec970b30
sbaecker2005 <144857264+sbaecker2005@users.noreply.github.com>
2024-05-23 14:36:44 -0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -5,17 +5,13 @@ int contar_vogais(char *string) {
     int total_vogais = 0;
     char *ptr = string;
 
-    // Percorre cada caractere na string até encontrar o caractere nulo de término '\0'
     while (*ptr != '\0') {
-        // Converte o caractere para minúsculo antes de verificar se é uma vogal
         char lower_char = tolower(*ptr);
         
-        // Verifica se o caractere é uma vogal
         if (lower_char == 'a' || lower_char == 'e' || lower_char == 'i' || lower_char == 'o' || lower_char == 'u') {
             total_vogais++;
         }
 
-        // Move para o próximo caractere
         ptr++;
     }
 
@@ -23,13 +19,10 @@ int contar_vogais(char *string) {
 }
 
 int main() {
-    char entrada[100]; // Assume que a entrada da string não terá mais de 100 caracteres
-
-    // Solicita ao usuário a entrada da string
+    char entrada[100]; 
     printf("Digite uma string: ");
     fgets(entrada, sizeof(entrada), stdin);
 
-    // Chama a função contar_vogais e exibe o resultado
     printf("Número de vogais na string: %d\n", contar_vogais(entrada));
 
     return 0;
