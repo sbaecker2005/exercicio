766321edca7814e91916835d145252d9db9ae2bf
sbaecker2005 <144857264+sbaecker2005@users.noreply.github.com>
2024-05-23 15:12:17 -0300

    Update ex8.c

--- ex8.c
+++ ex8.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Função para inverter um array
 int* inverterArray(int *array, int tamanho) {
-    int *arrayInvertido = (int*)malloc(tamanho * sizeof(int)); // Aloca memória para o novo array invertido
+    int *arrayInvertido = (int*)malloc(tamanho * sizeof(int)); 
 
-    if (arrayInvertido == NULL) { // Verifica se a alocação foi bem-sucedida
+    if (arrayInvertido == NULL) { 
         printf("Erro: Falha ao alocar memória.\n");
         return NULL;
     }
 
-    // Preenche o novo array invertido
+   
     for (int i = 0; i < tamanho; i++) {
         *(arrayInvertido + i) = *(array + tamanho - 1 - i);
     }
@@ -18,7 +17,6 @@ int* inverterArray(int *array, int tamanho) {
     return arrayInvertido;
 }
 
-// Função para imprimir um array
 void imprimirArray(int *array, int tamanho) {
     for (int i = 0; i < tamanho; i++) {
         printf("%d ", *(array + i));
@@ -33,13 +31,11 @@ int main() {
     printf("Array original: ");
     imprimirArray(arrayOriginal, tamanho);
 
-    // Chama a função para inverter o array
     int *arrayInvertido = inverterArray(arrayOriginal, tamanho);
     if (arrayInvertido != NULL) {
         printf("Array invertido: ");
         imprimirArray(arrayInvertido, tamanho);
 
-        // Libera a memória alocada para o array invertido
         free(arrayInvertido);
     }
 
