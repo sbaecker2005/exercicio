1154a083d7902b7ee6b8134bdca7edaa6dc22663
sbaecker2005 <144857264+sbaecker2005@users.noreply.github.com>
2024-05-24 19:35:57 -0300

    Update ex8.c

--- ex8.c
+++ ex8.c
@@ -9,7 +9,6 @@ int* inverterArray(int *array, int tamanho) {
         return NULL;
     }
 
-   
     for (int i = 0; i < tamanho; i++) {
         *(arrayInvertido + i) = *(array + tamanho - 1 - i);
     }
@@ -25,8 +24,21 @@ void imprimirArray(int *array, int tamanho) {
 }
 
 int main() {
-    int arrayOriginal[] = {1, 2, 3, 4, 5};
-    int tamanho = sizeof(arrayOriginal) / sizeof(arrayOriginal[0]);
+    int tamanho;
+
+    printf("Digite o tamanho do array: ");
+    scanf("%d", &tamanho);
+
+    int *arrayOriginal = (int*)malloc(tamanho * sizeof(int));
+    if (arrayOriginal == NULL) {
+        printf("Erro: Falha ao alocar memória.\n");
+        return 1;
+    }
+
+    printf("Digite os elementos do array:\n");
+    for (int i = 0; i < tamanho; i++) {
+        scanf("%d", &arrayOriginal[i]);
+    }
 
     printf("Array original: ");
     imprimirArray(arrayOriginal, tamanho);
@@ -39,5 +51,6 @@ int main() {
         free(arrayInvertido);
     }
 
+    free(arrayOriginal);
     return 0;
 }
